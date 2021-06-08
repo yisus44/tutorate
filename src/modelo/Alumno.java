33e3a67d666b37778b05d287bede0aa6bee4a2dd
yisus44 <55028090+yisus44@users.noreply.github.com>
2021-06-08 13:07:32 -0500

    cambiado el tipo tutor de ser string a int
    clase alumno

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -1,31 +1,31 @@
 
 package modelo;
 
-//bean
+//el id del tutor sera lo que unira las clases xd
 public class Alumno extends Usuario {
-    private String tutor;
+    private int tutor;
    
     //No añadimos un tutor en el constructor por si el usuario no quiere agregarlo cuando crea su cuenta
     public Alumno(int id, String nombre, String email, int edad, String contraseña) {
         super(id,nombre,email,edad,contraseña);
-        this.tutor="";
+        this.tutor=0;
     }
     
     public Alumno(String nombre, String email, int edad, String contraseña) {
         super(nombre,email,edad,contraseña);
-        this.tutor="";
+        this.tutor=0;
     }
 
     public Alumno() {
      super();
-     this.tutor="";
+     this.tutor=0;
     }
 
-    public String getTutores() {
-      return this.tutor="";
+    public int getTutores() {
+      return this.tutor;
     }
 
-    public void setTutor(String tutor) {
+    public void setTutor(int tutor) {
         this.tutor = tutor;
     }
   
