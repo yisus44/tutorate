c4a98eb429c20182d3ce8d7385fdafbaca77b741
sarahi <a20110332@ceti.mx>
2021-06-16 01:25:19 -0500

    uwu

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -3,28 +3,20 @@ package modelo;
 //el id del tutor sera lo que unira las clases xd
 public class Alumno extends Usuario {
     private int tutor;
-<<<<<<< HEAD
-=======
+    
     private int AlumnoID;
->>>>>>> yisus
    
     //No añadimos un tutor en el constructor por si el usuario no quiere agregarlo cuando crea su cuenta
     public Alumno(int id, String nombre, String email, int edad, String contraseña, int AlumnoID) {
         super(id,nombre,email,edad,contraseña);
-<<<<<<< HEAD
-=======
         this.AlumnoID=AlumnoID;
->>>>>>> yisus
         this.tutor=0;
     }
     
     public Alumno(String nombre, String email, int edad, String contraseña, int tutor) {
         super(nombre,email,edad,contraseña);
         this.tutor=0;
-<<<<<<< HEAD
         this.tutor=tutor;
-=======
->>>>>>> yisus
     }
 
     public Alumno() {
